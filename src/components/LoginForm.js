1ae4f61426cb0989198650fdd16a71ccd9e62d86
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-22 18:05:39 +0500

    Authentication with Firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed' });
+          });
+      })
+      .then(() => {
+
+      });
+  }
 
   render() {
     return (
@@ -29,8 +50,10 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={() => this.onButtonPress()}>
             Login
           </Button>
         </CardSection>
@@ -38,5 +61,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
