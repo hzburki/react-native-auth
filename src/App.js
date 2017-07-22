4576fa66b9b9e3fac6cd2c6997edd73003acb11a
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-22 19:39:43 +0500

    Authentication App Complete

--- src/App.js
+++ src/App.js
@@ -2,29 +2,69 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       authDomain: 'react-auth-2454c.firebaseapp.com',
-      apiKey: 'AIzaSyCc3IYjCIfZAuDJQJvSfWOElX3NCFbdw1o',
       databaseURL: 'https://react-auth-2454c.firebaseio.com',
+      apiKey: 'AIzaSyCc3IYjCIfZAuDJQJvSfWOElX3NCFbdw1o',
       projectId: 'react-auth-2454c',
       storageBucket: 'react-auth-2454c.appspot.com',
       messagingSenderId: '101955508379'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderLoginScreen() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Logout
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.centered}>
+            <Spinner />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText={'Authentication'} />
-        <LoginForm />
+        {this.renderLoginScreen()}
       </View>
     );
   }
 }
+
+const styles = {
+  centered: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 1,
+    alignSelf: 'center'
+  }
+};
 
 export default App;
