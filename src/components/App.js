ece4cd08c0c441e6d06df5a71ed85caf3b128e31
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-22 12:55:39 +0500

    Unified App Component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import react, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Hello Auth</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
