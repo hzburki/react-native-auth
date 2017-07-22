347f8f9b2013707a20bbf5f1ba7b1bee29fe8417
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-22 13:35:57 +0500

    Firebase Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
+
+import { Header } from './components/common';
 
 class App extends Component {
-  render () {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCc3IYjCIfZAuDJQJvSfWOElX3NCFbdw1o',
+      authDomain: 'react-auth-2454c.firebaseapp.com',
+      databaseURL: 'https://react-auth-2454c.firebaseio.com',
+      projectId: 'react-auth-2454c',
+      storageBucket: 'react-auth-2454c.appspot.com',
+      messagingSenderId: '101955508379'
+    });
+  }
+
+  render() {
     return (
       <View>
+        <Header headerText={'Authentication'} />
         <Text>Hello Auth</Text>
       </View>
     );
